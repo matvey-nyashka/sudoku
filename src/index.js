5f306827e5a3631f8d632def9aa4744c59eab422
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-09-06 00:20:42 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for(let i = 0; i < 9; i++){
+    for(let j = 0; j < 9; j++){
+      if(matrix[i][j] === 0){
+        for(let k = 1; k <= 9; k++){
+          if(number(matrix, i, j, k)){
+            matrix[i][j] = k;
+            if(solveSudoku(matrix)){
+              return matrix;
+            } else {
+              matrix[i][j] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return matrix;
+}
+
+function number(matrix, row, column, num){
+  for(let index = 0; index < 9; index++){
+    let x = 3 * Math.floor(row / 3) + Math.floor(index / 3);
+    let y = 3 * Math.floor(column / 3) +index % 3;
+    if(matrix[row][index] === num || matrix [index][column] === num || matrix[x][y] === num){
+      return false;
+    }
+  }
+  return true;
 }
